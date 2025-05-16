e2a878358c123418f1729a717bfc5b8c1e0dd683
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-05-16 20:08:20 -0300

    Update pesquisa-binaria.py

--- Pesquisa-binária/pesquisa-binaria.py
+++ Pesquisa-binária/pesquisa-binaria.py
@@ -1,1 +1,17 @@
+def pesquisa_binaria (item,lista):
+    baixo = 0
+    alto = len(lista)-1
+
+    while baixo <= alto:
+        meio = (baixo + alto)//2
+        chute = lista[meio]
+
+        if chute < meio:
+    baixo = 0 + 1
+else:
+    alto = len(lista)
+
+
+minha_lista = [9,11,13,15,17,19,21]
+
 
