2590cf577a9da620bde0bba37575df3579a2e96b
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-12 17:00:27 -0300

    Update gestao-de-obras.py

--- estruturas-de-dados/gestao-de-obras.py
+++ estruturas-de-dados/gestao-de-obras.py
@@ -1,12 +1,14 @@
 #Funcionalidades:
-# Selecionar/Criar obra do dia (pode ser só um nome ou código).
+# Selecionar/Criar obra do dia (nome do lugar).
 # Cadastrar funcionários presentes no dia, com valor da diária. 
 # Cadastrar gastos diversos, como materiais ou transporte (valor + descrição).
 # Exibir relatório com:
 # Total gasto com funcionários,
 # Total de outros gastos,
 # Total geral.
-# 📁 Organização sugerida:
-# Use classes: Obra, Funcionario, Gasto.
-# Armazene dados do dia numa lista ou dicionário.
-# No final, use print() para o resumo dos custos.
+# Permitir cadastro diario
+# Acesso a todos os gastos cadastrados ate o momento
+# Organização sugerida:
+# Usar classes: Obra, Funcionario, Gasto.
+# Armazenar dados do dia numa lista ou dicionário.
+# No final, usar print() para o resumo dos custos.
