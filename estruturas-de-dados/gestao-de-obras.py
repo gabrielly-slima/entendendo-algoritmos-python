dd21e93a135512653c4c3ee3bbcf55e8945cae06
Gabrielly Santos Lima <gsantoslima499@gmail.com>
2025-06-15 13:47:11 -0300

    Update gestao-de-obras.py

--- estruturas-de-dados/gestao-de-obras.py
+++ estruturas-de-dados/gestao-de-obras.py
@@ -14,6 +14,7 @@
 # No final, usar print() para o resumo dos custos.
 import re
 import hashlib
+import datetime
 
 class Usuario:
     def __init__(self,nome,email,senha):
@@ -22,9 +23,9 @@ class Usuario:
         self.senha = senha
 
 class Obra:
-    def __init__(self):
-        self.lugar = []
-        self.dia = []
+    def __init__(self,lugar,dia):
+        self.lugar = lugar
+        self.dia = dia
     
     #def adicionar_obra(self):
 
@@ -35,39 +36,45 @@ class Sistema:
         self.obras = []
         self.gastos = []
     
+    def menu_de_operacoes(self):
+        print("pegando")
+    
     def sign_in(self,nome,email,senha):
         cadastro = Usuario(nome,email,senha)
         self.usuarios.append(cadastro)
 
-
-
-def main():
-    print("GESTÃO OBRAS - BFX\n")
-    print("LOG-IN")
-    nome = input("Digite seu nome:")
-
-    while True:
-        email = input("Digite seu e-mail:")
-       
-        #.+	Pelo menos um caractere (qualquer) antes do @ | @	O caractere "@" obrigatório | .+	Pelo menos um caractere depois do @ | \.com	Literalmente ".com" (\. escapa o ponto) | $	Fim da string — ou seja, o .com tem que estar no final
-        validacao = re.search(r".+@.+\.com$",email) 
-        if validacao:
-            senha = input("Digite sua senha:")
+    
+    
+    def log_in(self):
+        while True:
+            email = input("Digite seu e-mail: ").strip()
+            senha = input("Digite sua senha: ").strip()
             senha_hash = hashlib.sha256(senha.encode()).hexdigest()
-            sistema = Sistema()
-            sistema.sign_in(nome,email,senha_hash)
-            print(f"{nome}, bem-vinda a GESTÃO OBRAS - BFX")
-            try:
-                menu_principal = int(input("MENU PRINCIPAL\n1. Adicionar obra\n2.Ver Relatórios\n3. Consultar gastos"))
-                if menu_principal == 1:
-                    obra = Obra(lugar,dia)
-                    sistema.obras.append(obra)
-            except ValueError:
-                print("bosta")
-        else:
-            print("E-mail inválido. Tente novamente")  
 
-            
-            
 
-main()
+            for usuario in self.usuarios:
+                if usuario.email == email and usuario.senha == senha_hash:
+                    print(f"Bem-vindo(a) de volta a GESTÃO OBRAS - BFX, {usuario.nome}!")
+                    self.menu_de_operacoes(usuario)
+                    return  # Sai da função log_in, login bem-sucedido
+
+                elif usuario.email == email and usuario.senha != senha_hash:
+                    print("Senha incorreta. Tente novamente.")
+                    continue
+
+                else:
+                    cadastrar = input("E-mail não cadastrado. Deseja se cadastrar? (SIM ou NÃO): ").upper().strip()
+                    if cadastrar == "SIM":
+                    self.sign_in()
+                    
+                    elif cadastrar == "NÃO":
+                        print("Encerrando sistema...")
+                        break
+            
+def main():
+    try:
+        sistema = Sistema()
+        sistema.log_in()
+    except ValueError:
+        print("ruik")
+    
